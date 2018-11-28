b05b384811d717af62a43a096afc9b1bf982d38d
tolgahanuzun <mail@tolgahanuzun.com>
2018-11-28 16:50:08 +0300

    Adding blacklist_add view.

--- dpoll/curation/urls.py
+++ dpoll/curation/urls.py
@@ -1,8 +1,20 @@
-from django.urls import path
+from django.urls import path, register_converter
 
 from . import views
 
-urlpatterns = [
-    path('blacklist/', views.index, name='black_list'),
+# https://stackoverflow.com/questions/48867977/django-2-url-path-matching-negative-value
+# Added to detect negative numbers.
+class NegativeIntConverter:
+    regex = '-?\d+'
+    def to_python(self, value):
+        return int(value)
+    def to_url(self, value):
+        return '%d' % value
 
+
+register_converter(NegativeIntConverter, 'negint')
+
+urlpatterns = [
+    path('blacklist/', views.index, name='blacklist'),
+    path('blacklist/@<str:user>/<negint:expire_day>/', views.add, name='blacklist_add'),
 ]
--- dpoll/curation/views.py
+++ dpoll/curation/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
 
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
 
+from polls.models import User
 from .models import Blacklist
+
+members = [ 'emrebeyler', 'isnochys', 'bluerobo', 'tolgahanuzun', 'admin']
 
 
 def index(request):
-    blacklist = Blacklist.objects.all()
+    blacklist = Blacklist.objects.filter(expires_at__gte=datetime.now())
 
     return render(request, "blacklist.html", {"blacklist": blacklist})
+
+
+def add(request, user, expire_day):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    if request.user.username not in members:
+        return HttpResponse('You are not authorized!')
+
+    get_user = User.objects.filter(username=user)
+    if not get_user:
+        return HttpResponse('User not found.')
+    
+    if expire_day < 0:
+        expire_day = None;
+    elif expire_day == 0:
+        return HttpResponse('Not funny...')
+    else:
+        expire_day = datetime.now() + timedelta(days=expire_day)
+    try:
+        Blacklist.objects.create(user=get_user.first(), expires_at=expire_day)
+    except:
+        raise Http404
+
+    return HttpResponse('Record created.')
